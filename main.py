57757c81f6bda7673cd75e94eb85e22fab32b1f5
katemcasto <82923019+katemcasto@users.noreply.github.com>
2022-12-15 02:59:43 -0500

    Added grid

--- main.py
+++ main.py
@@ -6,11 +6,20 @@ pygame.init()
 
 SW, SH = 800, 800
 
+BLOCK_SIZE = 50
 FONT = pygame.font.get_default_font()
 
 screen = pygame.display.set_mode((800, 800))
 pygame.display.set_caption("Snake!")
 clock = pygame.time.Clock()
+
+def drawGrid():
+    for x in range(0, SW, BLOCK_SIZE):
+        for y in range(0, SH, BLOCK_SIZE):
+            rect = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
+            pygame.draw.rect(screen, "#3c3c3b", rect, 1)
+
+drawGrid()
 
 while True: 
     for event in pygame.event.get():
